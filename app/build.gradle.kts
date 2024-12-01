d3fbf309fceab546d4ec72c6a838860b1b6f30b8
tmddn7475 <junseungu74@naver.com>
2024-12-01 12:57:09 +0900

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ithink.dailytodo"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.5"
+        versionCode = 8
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
